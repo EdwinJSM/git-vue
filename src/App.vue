01db2331bca6a30b868f98ec40d77f4d323bee04
polanco15 <55861429+polanco15@users.noreply.github.com>
2023-08-08 10:47:42 -0400

    Cambios en app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Malena no se baña"/>
 </template>
 
 <script>
@@ -20,7 +20,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #ff041d;
   margin-top: 60px;
 }
 </style>
