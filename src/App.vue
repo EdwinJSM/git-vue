a6d62f55d4bb3bc608fe93660e1e567099c19074
polanco15 <55861429+polanco15@users.noreply.github.com>
2023-08-09 13:26:34 -0400

    Nuevo componente tasks

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,105 @@
 <template>
    <div class="container">
  <HeaderHeader title="Task Tracker"/>
+  <TasksTasks :Tasks="Tasks"/>
    </div>
 </template>
 
 <script>
 import HeaderHeader from './components/HeaderHeader.vue';
+import TasksTasks from './components/TasksTasks.vue';
 
 export default {
   name: 'App',
   components: {
-  HeaderHeader
+  HeaderHeader,
+  TasksTasks
+
+  },
+  data(){
+    return{
+      Tasks: []
+    }
+},
+  created(){
+    this.Tasks =[
+    {
+      id: 1,
+      text: 'tareas1',
+      day: 'december 25 at 9:00pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'tareas2',
+      day: 'december 25 at 9:00pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'tareas3',
+      day: 'december 25 at 9:00pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'tareas4',
+      day: 'december 25 at 9:00pm',
+      reminder: true,
+    },
+    ]
   },
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #ff041d;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: 'Poppins', sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+
 .btn {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
   display: inline-block;
-  font-size: 16px;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.btn:active {
+  transform: scale(0.98);
+}
+
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ src/components/TasksTasks.vue
@@ -0,0 +1,14 @@
+<template>
+<div :key="Tasks.id" v-for="Tasks in Tasks">
+    <h3>{{ Tasks.text }}</h3>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'TasksTasks',
+    props :{
+        Tasks: Array,
+    }
+}
+</script>
